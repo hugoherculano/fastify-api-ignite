bb7706dfb9f5d77d1c1a1e7114ef79ceecf37791
hugoherculano <hugohercullano@gmail.com>
2023-04-01 16:31:56 -0300

    insert migrations from the database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import fastify from "fastify";
+import crypto from "node:crypto";
 import { knex } from "./database";
 
 const app = fastify();
 
 app.get('/hello', async () => {
-    const tables = await knex('sqlite_schema').select('*');
+    const transactions = await knex("transactions")
+    .where("amount", 500)
+    .select("*");
 
-    return tables
+    return transactions;
 });
 
 app.listen({ port: 3333 }).then(() => {
